6985e742e5c97cbbcd94a57a642ccd5a3efcec2d
Jason Orendorff <jason.orendorff@gmail.com>
2015-08-03 16:46:34 -0500

    algebraic-data-types: update examples

--- algebraic-data-types/Examples.hs
+++ algebraic-data-types/Examples.hs
@@ -4,9 +4,12 @@
 module Examples where
 import Prelude hiding (Bool, False, True)
 
+
+-- Example: Bool
 data Bool = False | True
 
 
+-- Example: TimeUnit
 data TimeUnit = Year | Month | Day | Hour | Minute | Second
 
 unitToEnglish Year = "years"
@@ -17,11 +20,12 @@ unitToEnglish Minute = "minutes"
 unitToEnglish Second = "seconds"
 
 
+-- Example: RoughTime
 data RoughTime = InThePast Int TimeUnit
                | JustNow
                | InTheFuture Int TimeUnit
 
-when = InThePast 27 Day
+when = InThePast 27 Day      -- "27 days ago"
 
 toEnglish :: RoughTime -> String
 toEnglish JustNow = "just now"
@@ -31,6 +35,7 @@ toEnglish (InTheFuture num unit) =
   show num ++ " " ++ unitToEnglish unit ++ " from now"
 
 
+-- Example: JSON
 data JSON = JNull
           | JBool Bool
           | JNum Float
@@ -39,12 +44,22 @@ data JSON = JNull
           | JObject [(String, JSON)]
 
 
+-- Example: Maybe
 data Maybe a = Nothing | Just a
 
 
+-- Example: Tree
 data Tree a = Empty | Node (Tree a, a, Tree a)
 
 
+-- Product type example: LunchSpecial
+data Curry = Red | Green | Panang | Massaman  --  4 curries
+data Stuff = Chicken | Pork | Tofu            --  3 "meats"
+
+type LunchSpecial = (Curry, Stuff)            -- 12 combinations
+
+
+-- Infinity!
 data Nat = Zero | Succ Nat
   deriving (Show, Eq, Ord)
 
